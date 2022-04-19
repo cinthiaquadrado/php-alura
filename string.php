e970c6a5e9e15eb229e04c9c3a6caf4133b07b7b
Cinthia Quadrado <92656147+cinthiaquadrado@users.noreply.github.com>
2022-04-19 15:46:28 -0300

    Entendendo um pouco mais de strings

---
+++ string.php
@@ -0,0 +1,11 @@
+<?php
+
+$idade = 21;
+
+echo "Olá, mundo".PHP_EOL;
+echo "Eu tenho \"$idade\" anos";
+
+echo PHP_EOL.PHP_EOL;
+
+echo "Você só pode entrar se tiver 18 anos\n";
+echo "Você tem $idade. Pode entrar";
